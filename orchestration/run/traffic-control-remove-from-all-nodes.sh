d73cc2f2865073df48e7cfaaecddcca03949bc3e
Vanlightly <vanlightly@gmail.com>
2020-09-16 01:34:02 -0700

    Fixed bug in traffic control removal script

--- orchestration/run/traffic-control-remove-from-all-nodes.sh
+++ orchestration/run/traffic-control-remove-from-all-nodes.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-set -e
-
 KEY_PAIR=$1
 NODE_NUMBER=$2
 CLUSTER_SIZE=$3
@@ -12,4 +10,5 @@ for NODE in $(seq $NODE_NUMBER $LAST_NODE)
 do
     BROKER_IP=$(aws ec2 describe-instances --filters "Name=tag:inventorygroup,Values=benchmarking_rabbitmq${NODE}_${RUN_TAG}" --query "Reservations[*].Instances[*].PublicIpAddress" --output=text)
     ssh -i "~/.ssh/$KEY_PAIR.pem" -o "StrictHostKeyChecking=no" -o "UserKnownHostsFile=/dev/null" ubuntu@$BROKER_IP sudo tc qdisc del dev ens5 root > /dev/null 2>&1
+    echo "Removed traffic control from $BROKER_IP"
 done
